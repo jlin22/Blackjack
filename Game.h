ef97ee11126e9471f3116868cff0d3aeac22a1c4
John Lin <jlin22@stuy.edu>
2018-07-12 10:48:37 -0400

    game over conditions finished

--- Game.h
+++ Game.h
@@ -7,14 +7,21 @@ public:
     std::string declare_turn(); 
     bool parse_command(std::string comm);
     void display_board();
-    std::string ask_command();
+    void ask_command();
     bool game_over();
     void run_game();
     void determine_winner();
+    void print_winner();
 private:
     std::vector<Player> players;
     Deck d; 
     int turn = 0;
     int num_players;
 }; 
+
+/* Issues to contend with:
+Conditions for game over (bust, stay) done
+Skipping turns for stay
+Who won (whoever busts loses, if both stay then highest value)
+*/
 
